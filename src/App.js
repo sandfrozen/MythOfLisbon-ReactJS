ed3681ca903dd25db1422f8d15856b512e76cdbf
Tomek Buslowski <tbuslowski@gmail.com>
2018-10-15 16:48:16 +0100

    router

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import './App.css'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+import Error from './components/Error'
+import Nagivation from './components/Nagivation'
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <BrowserRouter>
+        <Fragment>
+          <Nagivation />
+          <Switch>
+            <Route path='/' component={Home} exact />
+            <Route path='/about' component={About} />
+            <Route path='/contact' component={Contact} />
+            <Route component={Error} />
+          </Switch>
+        </Fragment>
+      </BrowserRouter>
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/components/Error.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Error = () => {
+  return (
+    <div>
+      This path is incorrect..
+    </div>
+  )
+}
+
+export default Error
